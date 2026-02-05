aba8e7e3e2a791ae68b8c6330ce7b32c20ba44af
Gopal Shukla <gopalshukla0018@gmail.com>
2026-02-05 07:25:09 +0530

    printRactangularStar function based

--- patternPrinting/01_rectangularStarPattern.js
+++ patternPrinting/01_rectangularStarPattern.js
@@ -1,10 +1,22 @@
-let row = 4;
-let col = 4;
+// let row = 4;
+// let col = 4;
 
-for (let i = 0; i < row; i++) {
-  let output = "";
-  for (let j = 0; j < col; j++) {
-    output += "* ";
+// for (let i = 0; i < row; i++) {
+//   let output = "";
+//   for (let j = 0; j < col; j++) {
+//     output += "* ";
+//   }
+//   console.log(output);
+// }
+
+function printRactangularStar(rows, col) {
+  for (let i = 0; i < rows; i++) {
+    output = "";
+    for (let j = 0; j < col; j++) {
+      output += "* ";
+    }
+    console.log(output);
   }
-  console.log(output);
 }
+
+printRactangularStar(4, 5);
