7a59b02ae7877c7b63223499f92160268103e68d
Gopal Shukla <gopalshukla0018@gmail.com>
2026-02-05 07:20:38 +0530

    02_rightAngledTrianglePattern function based

--- patternPrinting/02_rightAngledTrianglePattern.js
+++ patternPrinting/02_rightAngledTrianglePattern.js
@@ -1,9 +1,26 @@
-let row=5;
+// let row=5;
 
-for(let i=0;i<row;i++){
-    output="";
-    for(let j=0;j<i;j++){
-        output += "* "
+// for(let i=0;i<row;i++){
+//     output="";
+//     for(let j=0;j<i;j++){
+//         output += "* "
+//     }
+//     console.log(output)
+// }
+
+
+
+function printTriangle(rows) {
+  for (let i = 1; i <= rows; i++) {
+    let output = "";
+
+    for (let j = 1; j <= i; j++) {
+      output += "* ";
     }
-    console.log(output)
+
+    console.log(output);
+  }
 }
+
+// input
+printTriangle(5);
