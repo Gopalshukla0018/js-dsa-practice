870e7c34939b281affd9678b88bb696844bc44b4
Gopal Shukla <gopalshukla0018@gmail.com>
2026-04-05 21:20:45 +0530

    Refactor removeDuplicates function for clarity and correctness

--- RemoveDuplicates.js
+++ RemoveDuplicates.js
@@ -24,19 +24,16 @@
 
 // console.log(removeDuplicates(arr));
 
-
-
-var removeDuplicates = function(nums) {
+var removeDuplicates = function (nums) {
   let x = 0;
-  for (let i = 1; i <= nums.length; i++) {
-    if (nums[i]!==nums[x]) {
-      x=x+1;
-      nums[x]=nums[i]
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== nums[x]) {
+      x = x + 1;
+      nums[x] = nums[i];
     }
   }
-  return x; 
+  return nums.slice(0, x + 1);
 };
 
-
-let nums = [0,0,1,1,1,2,2,3,3,4]
+let nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
 console.log(removeDuplicates(nums));
