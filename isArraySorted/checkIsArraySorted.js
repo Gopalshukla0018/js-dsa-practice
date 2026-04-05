04a7e1a5d7361dc573a377990a32d235d3fb3c80
Gopal Shukla <gopalshukla0018@gmail.com>
2026-04-05 20:51:16 +0530

    solve new problem: check if an array is sorted in ascending order.

---
+++ isArraySorted/checkIsArraySorted.js
@@ -0,0 +1,12 @@
+let arr = [1, 2, 3, 113, 516, 999];
+
+function isSorted(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > arr[i + 1]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(isSorted(arr));
